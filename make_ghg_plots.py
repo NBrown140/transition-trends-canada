0fbc433db5fa7c1f94a7ac82db7543afcd810061
Nicolas Brown <nicolas.brown@overstory.com>
2024-05-15 20:29:07 -0400

    Rename ghg plots title

--- make_ghg_plots.py
+++ make_ghg_plots.py
@@ -9,7 +9,7 @@ def plot_monthly_yearly_growth_rate(url: str, header: int, gas: str, gas_unit: s
     df["average_diff12"] = df["average"].diff(12)
     df["average_diff12_rollmean12"] = df["average"].diff(12).rolling(12, center=True).mean()
     fig, ax = plt.subplots()
-    ax.set_title(f"Global Monthly Mean Atmospheric {gas} - Yearly Growth Rate")
+    ax.set_title(f"Global Monthly Year-Over-Year Change - {gas}")
     ax.set_ylabel(f"[{gas_unit}]", style='italic')
     # for i, row in df_annual_gr.iterrows():
     #     if i == len(df_annual_gr)-1:  # Only add legend label once
@@ -17,7 +17,7 @@ def plot_monthly_yearly_growth_rate(url: str, header: int, gas: str, gas_unit: s
     #     else:
     #         kwargs = {}
     #     hline = ax.hlines(row["ann inc"], row["year"], row["year"] + 1, color="grey", **kwargs)
-    ax.scatter(df["decimal"], df["average_diff12"], s=1, c="#cdcdcd", label="YoY Growth Rate")
+    ax.scatter(df["decimal"], df["average_diff12"], s=1, c="#cdcdcd", label="YoY Change")
     ax.plot(df["decimal"], df["average_diff12_rollmean12"], linewidth=1, color=color, label="12-month Running Mean")
     ax.legend(loc="lower right")
     ax.text(0.71, -0.12, f"Updated on: {datetime.now().strftime('%Y-%m-%d')}", size=8, transform=plt.gca().transAxes)
